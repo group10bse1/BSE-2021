b3d8a31866262352eca81cbe2f51b2b2948170c1
group10bse1 <group10bse@gmail.com>
2021-04-17 09:29:52 -0700

    exercise2

--- src/chapter4/exercise2.py
+++ src/chapter4/exercise2.py
@@ -1,11 +1,12 @@
-def investiment(initial, rate, time, number):
+# calculating final value of investment
+def investment(initial, rate, time, number):
     print(initial*(1 + rate/number)**(time*number))
 
 
-c = int(input("enter initial: "))
+c = int(input("enter initial amount: "))
 r = float(input("enter rate: "))
 t = int(input("enter time: "))
-n = int(input("enter number: "))
-investiment(c, r, t, n)
+n = int(input("enter number of times: "))
+investment(c, r, t, n)
 
 
