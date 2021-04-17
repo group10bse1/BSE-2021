29d7899ffd0088ecaeb8ec7cf646e04eb779495f
group10bse1 <group10bse@gmail.com>
2021-04-17 04:54:45 -0700

    exercise6

---
+++ src/chapter4/exercise6.py
@@ -0,0 +1,12 @@
+# computing pay and overtime
+def computepay(hours, rate):
+    if hours <= 40:
+        return hours*rate
+    overtime = hours-40
+    return 40*rate + overtime * 1.5 * rate
+
+
+hrs = int(input("enter hours: "))
+rte = int(input("enter rate: "))
+print(computepay(hrs, rte))
+# printing pay finally
