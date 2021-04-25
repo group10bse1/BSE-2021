926b1bc51d3b1bb2d902ddeec388fbb2ae5d2621
group10bse1 <group10bse@gmail.com>
2021-04-25 07:14:11 -0700

    excerise 3 continuation

--- src/chapter5/exercise3.py
+++ src/chapter5/exercise3.py
@@ -3,7 +3,7 @@ Dimes = 25
 Quater = 25
 Ones = 0
 Fives = 0
-balance = None
+balance = 0
 
 while True:
     print('Stock is as follows')
@@ -26,13 +26,64 @@ while True:
 
             while True:
                 if new_price > 0 and (new_price % 0.5 == 0) and balance > 0:
-                    print("'n' - deposit a nickel\n")
-                    print("'d' - deposit a dime\n")
-                    print("'q' - deposit a quarter\n")
-                    print("'o' - deposit a one dollar bill\n")
-                    print("'f' - deposit a five dollar bill\n")
-                    print("'c' - Cancel the purchase\n")
-                    print(f"Payment due is: " + str(balance))
+                    print("'n' - deposit a nickel")
+                    print("'d' - deposit a dime")
+                    print("'q' - deposit a quarter")
+                    print("'o' - deposit a one dol0lar bill")
+                    print("'f' - deposit a five dollar bill")
+                    print("'c' - Cancel the purchase")
+                    print(f"Payment due is: {int(balance // 100)} dollars and {int(balance % 100)} cents\n")
                     menu = input("Enter any of the above..: ")
+                    if menu == "d":
+                        balance -= 0.1 * 100
+                        print(balance)
+                        Dimes += 1
+
+
+                    elif menu == "n":
+                        balance -= 0.05 * 100
+                        Nickel += 1
+
+                    elif menu == "q":
+                        balance -= 0.1 * 100
+                        Quater += 1
+                    elif menu == "o":
+                        balance -= 1 * 100
+                        Ones += 1
+
+                    elif menu == "f":
+                        balance -= 5 * 100
+                        Fives += 1
+                    elif menu == "c":
+                        break
+                    else:
+                        print(menu + " is not valid selection")
+                elif new_price < 0:
+                    print("illegal entry...")
+                elif balance <= 0:
+                    change = float(balance * -1)
+
+                    if 0.05 <= change <= 0.09:
+                        n = round(change / 0.05, 2)
+                        Nickel -= n
+                        if Nickel >= n:
+                            print(f"\nChange is: {n} Nickel")
+                        elif Nickel != 0:
+                            rem = n - Nickel
+                            print('\nThe nickles are not enough\n'
+                                  ''f"Pick {Nickel} from the dispensor\n"
+                                  f" Pick {rem} as the balance from...")
+                        else:
+                            print('')
+                    elif 0.1 <= change <= 0.24:
+                        d = change / 0.1
+                        print(f"\nChange is: {d} dollars")
+                    elif 0.25 <= change <= 0.9:
+                        q = change / 0.25
+                        print(f"\nChange is: {q} Quaters")
+                    break
+                else:
+                    print("Enter correct price foramt")
+                    break
         except:
             print("error")
