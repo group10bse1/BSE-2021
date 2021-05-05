b2038c4920a035f5aa61a24e61f4ef34fcd90626
group10bse1 <group10bse@gmail.com>
2021-05-05 09:12:34 -0700

    getting meter readings and meter gallons

--- mid_term/project.py
+++ mid_term/project.py
@@ -1,7 +1,17 @@
-# entering customer's code repeatedily.
+# entering customer's code repeatedly.
 while True:
     c_code = input('Enter customer code: ')
     if c_code == "r" or c_code == "c" or c_code == "i":
-        print("customer code", c_code)
+        f_reading = input("Enter beginning meter reading: ")
+        e_reading = input("Enter ending meter reading: ")
+        if int(e_reading) < int(f_reading):
+            em_reading = int(e_reading) + 1000000000
+        else:
+            em_reading = int(e_reading)
+        gallon = (int(em_reading)-int(f_reading))/10
+        print("customer code: ", c_code)
+        print("Beginning meter reading: ", f_reading)
+        print("Ending meter reading: ", e_reading)
+        print(gallon)
     else:
         quit()
