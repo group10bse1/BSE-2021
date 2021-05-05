666f42f74c102ddaad1302e715585e589220c449
group10bse1 <group10bse@gmail.com>
2021-05-05 10:43:34 -0700

    calculating amount billed

--- mid_term/project.py
+++ mid_term/project.py
@@ -9,9 +9,27 @@ while True:
         else:
             em_reading = int(e_reading)
         gallon = (int(em_reading)-int(f_reading))/10
+        if c_code == "r":
+            bill = format((5 + 0.0005 * gallon), ".2f")
+        elif c_code == "c":
+            if gallon <= 4000000:
+                bill = format(1000, ".2f")
+            else:
+                a_gallon = gallon - 4000000
+                bill = format((a_gallon * 0.00025 + 1000), ".2f")
+        else:
+            if gallon <= 4000000:
+                bill = format(1000, ".2f")
+            elif gallon <= 10000000:
+                bill = format(2000, ".2f")
+            else:
+                a_gallon = gallon - 10000000
+                bill = format((a_gallon * 0.00025 + 2000), ".2f")
+        # displaying outputs.
         print("customer code: ", c_code)
         print("Beginning meter reading: ", f_reading)
         print("Ending meter reading: ", e_reading)
         print("Gallons of water used: ", gallon)
+        print("Amount billed: "+"$" + str(bill))
     else:
         quit()
