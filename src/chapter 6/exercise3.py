73c65ead943e97def285e02218a716bce340f4da
group10bse1 <group10bse@gmail.com>
2021-05-16 08:29:40 -0700

    exercise1

---
+++ src/chapter 6/exercise3.py
@@ -0,0 +1,9 @@
+def count(string_p,letter_p):
+    count_x = 0
+    for letter in string_p:
+        if letter == letter_p:
+            count_x = count_x + 1
+    return count_x
+word = input('Enter word:')
+letter_x = input('Enter letter:')
+print(count(word, letter_x))
---
+++ src/chapter6/exercise1.py
@@ -0,0 +1,5 @@
+fruit = "banana"
+index = (len(fruit)-1)
+while index < len(fruit) and index >= 0:
+    print(fruit[index])
+    index = index - 1
